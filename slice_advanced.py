43ceb2de9fdc322d41e08d0069e8641d1d8c4171
eduudecebalcuza <162722197+eduudecebalcuza@users.noreply.github.com>
2024-04-05 12:53:52 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,7 +1,3 @@
 def slice_advanced():
     name = input()
     print (name[4::2])
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
